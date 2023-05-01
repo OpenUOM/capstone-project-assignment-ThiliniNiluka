27f5cccbaca53489f4349a7c6e27ed637a0515d5
bridge-challenges <thilininilukakumari@gmail.com>
2023-05-01 07:17:21 +0530

    port 8080 is used for another project

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ if(process.env.NODE_ENV === "test"){
     );
   });
 }else{
-  server.listen(8080, () => {
+  server.listen(8888, () => {
     console.log(
       "Capstone Project Backend is running on http://localhost:8888"
     );
